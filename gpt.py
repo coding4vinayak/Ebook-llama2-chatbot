99726f55c806319c53bd9805a79a68bbed72bf1c
Vinayak Sonawane <coding4vinayak@gmail.com>
2024-01-29 14:42:08 +0530

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="vinayak-Ebook gpt bot")
 
 with st.sidebar:
      st.title('coding4vinayak gpt bot')
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
